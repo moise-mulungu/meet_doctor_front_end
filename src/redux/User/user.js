53c63f2da1163de8a85c2d388ddead00a9a01e83
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-15 03:47:19 +0430

    Create user reducer

---
+++ src/redux/User/user.js
@@ -0,0 +1,36 @@
+const LOGIN_USER= 'MeetDoctorFrontEnd/User/LOGIN_USER';
+const initialState = { status: 'Not Loged in', data: [] };
+
+
+export const success = (user) => ({
+    type: LOGIN_USER,
+    user,
+  });
+
+  export const loginUser = () => async (dispatch) => {
+    fetch('http://127.0.0.1:3000/v1/users', {
+      method: 'GET',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+      
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch(success(res));
+         })
+      .catch((error) => { throw error; });
+  
+  };
+
+  const UserReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case LOGIN_USER:
+        return { data: action.user, status: 'Loged In successfully' };
+        default:
+        return state;
+    }
+  };
+  
+  export default UserReducer;
