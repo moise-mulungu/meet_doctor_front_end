36b551a252ee7f9b1cfa0e66a4bfeb4f8ca8d2a8
samiullah <bahadursamiullah@gmail.com>
2022-08-17 16:15:50 +0430

    Add loginpage in pages

--- src/Pages/loginPage.jsx
+++ src/Pages/loginPage.jsx
@@ -1,23 +1,31 @@
-
-import React from 'react';
+import React from 'react'
+import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
 import { loginUser } from '../redux/User/user';
 
-const HomePage = () => {
-  const dispatch = useDispatch();
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button
-        type="button"
-        className="microphone"
-        label="microphone"
-        onClick={() => dispatch(loginUser())}
-      >
-        login user
-      </button>
-    </div>
-  );
-};
 
-export default HomePage;
+const SignupPage = () => {
+   const navigate = useNavigate()
+   /* eslint-disable */
+
+    const dispatch = useDispatch();
+    const { register, handleSubmit } = useForm();
+    /* eslint-enable */
+
+     const onSubmit = (data) => (dispatch(loginUser(data)) ? navigate('/main') : null ) ;
+  
+  return (
+    <div>
+    <h1>Login </h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+     <input {...register('username')} placeholder="username" />
+
+      <input type="submit" name="Loing" />
+    </form>
+    </div>
+  )
+}
+
+export default SignupPage
