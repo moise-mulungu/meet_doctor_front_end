2262a29f94f8f4dabe54764e799361a7aa92b5b3
samiullah <bahadursamiullah@gmail.com>
2022-08-17 16:48:37 +0430

    Create configureStore

--- src/redux/configurStore.js
+++ src/redux/configurStore.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
 import UserReducer from './User/user';
 
 const rootReducer = combineReducers({
   User: UserReducer,
 });
 
-const store = createStore(
-  rootReducer, applyMiddleware(thunk, logger),
-);
+
 
 export default store;
