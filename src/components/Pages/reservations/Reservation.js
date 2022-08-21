bb49f6e84dfa2574950c77074ff3de4589cba3d0
moise <moisemlg90@gmail.com>
2022-08-21 07:23:42 +0200

    Add reservation reducer

--- src/components/Pages/reservations/Reservation.js
+++ src/components/Pages/reservations/Reservation.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { setReservation } from '../../actions/reservationActions';
+import { deleteReservation } from '../../../actions/reservationActions';
+import { setReservation } from '../../../actions/reservationActions';
+import { addReservation } from '../../../actions/reservationActions';
 
 
 const Reservations = () => {
   
   const [reservation, setReservation] = useState({
+    doctor_id: '',
     city: '',
     datetime: ''
   });
 
   const dispatch = useDispatch();
-  const { city, datetime } = reservation;
+  const reservations = useSelector(state => state.reservations);
+  const { doctor_id, city, datetime } = reservation;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(setReservation(city, datetime));
+    dispatch(setReservation(doctor_id, city, datetime));
   }
 
   const handleChange = (e) => {
@@ -28,10 +32,15 @@ const Reservations = () => {
   const cities = ['New York', 'Paris', 'London', 'Tokyo', 'Rome', 'Lome', 'Kinshasa', 'Kabul'];
   const datetimes = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
 
+
   return (
     <div className="reservations">
       <h1>Reservations</h1>
       <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="doctor_id">Doctor ID</label>
+          <input type="text" className="form-control" id="doctor_id" name="doctor_id" value={doctor_id} onChange={handleChange} />
+        </div>
         <div className="form-group">
           <label htmlFor="city">City</label>
           <select name="city" value={city} onChange={handleChange} className="form-control">
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,9 +3,11 @@ import { applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import userReducer from './users/user';
+import reservationReducer from './reservations/reservation';
 
 const reducer = combineReducers({
   user: userReducer,
+  reservation: reservationReducer
 });
 
 const store = configureStore({ reducer }, applyMiddleware(thunk, logger));
