e8b96de1bc595a20cf4ead4a6f4c6f4e0a723d62
moise <moisemlg90@gmail.com>
2022-08-22 14:59:13 +0200

    Add list all reservation functionalities

--- src/components/Pages/reservations/Reservation.js
+++ src/components/Pages/reservations/Reservation.js
@@ -1,63 +1,76 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteReservation } from '../../../actions/reservationActions';
-import { setReservation } from '../../../actions/reservationActions';
-import { addReservation } from '../../../actions/reservationActions';
+import { loadStorage } from "../../../storage/storage";
 
+const FETCH_RESERVATIONS = 'FETCH_RESERVATIONS';
+const initialState = {
+  reservations: []
+};
 
-const Reservations = () => {
-  
-  const [reservation, setReservation] = useState({
-    doctor_id: '',
-    city: '',
-    datetime: ''
-  });
-
-  const dispatch = useDispatch();
-  const reservations = useSelector(state => state.reservations);
-  const { doctor_id, city, datetime } = reservation;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(setReservation(doctor_id, city, datetime));
-  }
-
-  const handleChange = (e) => {
-    setReservation({
-      ...reservation,
-      [e.target.name]: e.target.value
-    });
-  }
-
-  const cities = ['New York', 'Paris', 'London', 'Tokyo', 'Rome', 'Lome', 'Kinshasa', 'Kabul'];
-  const datetimes = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
-
-
-  return (
-    <div className="reservations">
-      <h1>Reservations</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="doctor_id">Doctor ID</label>
-          <input type="text" className="form-control" id="doctor_id" name="doctor_id" value={doctor_id} onChange={handleChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="city">City</label>
-          <select name="city" value={city} onChange={handleChange} className="form-control">
-            {cities.map(city => <option key={city}>{city}</option>)}
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="datetime">Datetime</label>
-          <select name="datetime" value={datetime} onChange={handleChange} className="form-control">
-            {datetimes.map(datetime => <option key={datetime}>{datetime}</option>)}
-          </select>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  );
-
+export const fetchReservations = () => {
+  return {
+    type: FETCH_RESERVATIONS
+  };
 }
 
-export default Reservations;
+export const fetchReservationsSuccess = (reservations) => {
+  return {
+    type: FETCH_RESERVATIONS,
+    payload: reservations
+  };
+}
+
+export const fetchReservationsFailure = (error) => {
+  return {
+    type: FETCH_RESERVATIONS,
+    payload: error
+  };
+}
+
+export const fetchReservationsAsync = () => {
+  return (dispatch) => {
+    const user = loadStorage();
+    if (user === null) {
+      return dispatch(fetchReservationsFailure('No user found'));
+    }
+    return fetch(`http://localhost:3000/reservations/${user.id}`)
+      .then(response => response.json())
+      .then(reservations => {
+        dispatch(fetchReservationsSuccess(reservations));
+      }).catch(error => {
+        dispatch(fetchReservationsFailure(error));
+      }
+      );
+  }
+}
+
+export const updateReservation = (reservation) => {
+  return (dispatch) => {
+    const user = loadStorage();
+    if (user === null) {
+      return dispatch(fetchReservationsFailure('No user found'));
+    }
+    return fetch(`http://localhost:3000/reservations/${user.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(reservation)
+    }).then(response => response.json())
+      .then(reservations => {
+        dispatch(fetchReservationsSuccess(reservations));
+      }).catch(error => {
+        dispatch(fetchReservationsFailure(error));
+      }
+      );
+  }
+}
+
+export const fetchReservationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_RESERVATIONS:
+      return {
+        ...state,
+        reservations: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+export default fetchReservationReducer;
