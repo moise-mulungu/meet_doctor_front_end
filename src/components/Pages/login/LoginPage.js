53bb4e86ae8524f716e2f5c3165f265b0dbd6640
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-22 15:08:50 +0000

    Styled ligin page

--- src/components/Pages/login/LoginPage.js
+++ src/components/Pages/login/LoginPage.js
@@ -4,40 +4,85 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../../redux/users/user';
 import { loadStorage } from '../../../storage/storage';
+import logImgDoc from '../../../images/img_doc_login.jpg';
 
 const HomePageCtn = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
 
   .ctn-card {
-    width: 50%;
-    height: 50%;
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    flex-direction: row;
+    align-self: center;
+    border: 1px solid #f9efef;
+    margin-top: 50px;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 5px 5px 75px rgba(136, 136, 136, 0.89);
+  }
+
+  .form_ctn {
+    width: 400px;
+    padding-left: 30px;
+    padding-right: 50px;
+  }
+
+  .login_txt {
+    text-align: center;
+    font-size: 1.5rem;
+    color: var(--text-black);
+  }
+
+  form {
     display: flex;
     flex-direction: column;
-    border: 1px solid #f9efef;
-    margin-top: 100px;
-    padding: 10px;
-    box-shadow: 5px 10px 18px #888888;
+    gap: 150px;
   }
-  
-  form{
+
+  label {
     display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+
+  .doctor_log_img {
+    width: 500px;
   }
 
   .login-title {
     align-self: center;
+    color: var(--green);
+    font-size: 2rem;
+    font-style: italic;
+  }
+
+  .username_field {
+    height: 50px;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+  .login-button {
+    background-color: var(--green);
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 300ms linear 0s;
   }
   
-  .username_field{
-    height: 40px;
-  }
-  
-  .login-button{
-    height: 40px;
-    width: 150px;
-    margin: auto;
+  .login-button:hover{
+    background-color: var(--green-darker);
   }
 `;
 
@@ -70,12 +115,19 @@ function LoginPage() {
 
   return (
     <HomePageCtn>
+      <h1 className="login-title">meet_doctor</h1>
       <div className="ctn-card">
-        <h1 className="login-title">meet_doctor</h1>
-        <form onSubmit={submitLogin}>
-          <input className="username_field" placeholder="Username" name="username" onChange={updateInput} />
-          <input className="login-button" type="submit" value="Login" />
-        </form>
+        <img className="doctor_log_img" src={logImgDoc} alt="login_doctor_picture" />
+        <div className="form_ctn">
+          <h5 className="login_txt">Log in</h5>
+          <form onSubmit={submitLogin}>
+            <label htmlFor="username">
+              Username:
+              <input className="username_field" id="username" placeholder="Username" name="username" onChange={updateInput} />
+            </label>
+            <input className="login-button" type="submit" value="Login" />
+          </form>
+        </div>
       </div>
 
     </HomePageCtn>
