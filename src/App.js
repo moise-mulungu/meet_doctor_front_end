37e30b7f695dee3eb4501e6f75ea1a45779a077c
samiullah <bahadursamiullah@gmail.com>
2022-08-22 13:53:30 +0430

    Add link to app page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import LoginPage from './components/Pages/login/LoginPage';
 import MainPage from './components/Pages/main/MainPage';
+import ReservationForm from './components/Pages/reserve/Reserve';
+import { fetchDoctors } from './redux/doctors/DoctorList';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDoctors());
+  }, []);
+
+  return (
+
     <Router>
       <Routes>
         <Route exact path="login" element={<LoginPage />} />
+        <Route path="/reserve" element={<ReservationForm />} />
         <Route path="*" element={<MainPage />} />
       </Routes>
     </Router>
-  </div>
-);
+  );
+};
 
 export default App;
