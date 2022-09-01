6fe916934b494350b2062296dc62f5831bf6c2b5
Cédric Kossi <lionel.kossi@gmail.com>
2022-09-01 01:27:15 +0000

    Add image to doctor details section

--- src/components/Pages/home/DoctorDetails.js
+++ src/components/Pages/home/DoctorDetails.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import React from "react";
 
 const DoctorDetailsCtn = styled.div`
   @media (min-width: 768px) {
@@ -20,8 +21,8 @@ const DoctorDetailsCtn = styled.div`
       flex-direction: column;
       justify-content: center;
     }
-    
-    .img-ctn{
+
+    .img-ctn {
       display: flex;
       flex-direction: column;
       justify-content: center;
@@ -38,8 +39,15 @@ const DoctorDetailsCtn = styled.div`
       border-radius: 100%;
       background: var(--img-background);
     }
-    
-    .doc-name{
+
+    .doc-img {
+      width: 150px;
+      height: 150px;
+      margin: auto;
+      border-radius: 100%;
+    }
+
+    .doc-name {
       align-self: center;
     }
 
@@ -63,8 +71,8 @@ const DoctorDetailsCtn = styled.div`
     .white {
       background: rgb(249, 255, 247);
     }
-    
-    .reserve-btn{
+
+    .reserve-btn {
       background-color: var(--green);
       color: white;
       align-self: center;
@@ -75,7 +83,8 @@ const DoctorDetailsCtn = styled.div`
       cursor: pointer;
       transition: all 300ms linear 0s;
       margin-top: 50px;
-      :hover{
+
+      :hover {
         background-color: var(--green-darker);
       }
     }
@@ -83,9 +92,9 @@ const DoctorDetailsCtn = styled.div`
 `;
 
 const DoctorDetails = (props) => {
-  const { detailsOpen } = props;
+  const {detailsOpen} = props;
   const navigate = useNavigate();
-  const { doctor } = props;
+  const {doctor} = props;
   const docDetails = JSON.parse(doctor);
 
   const openReserve = () => {
@@ -99,39 +108,43 @@ const DoctorDetails = (props) => {
       {
         docDetails
         && (
-        <div className="doc-info-ctn">
-          {/* Image part */}
-          <div className="img-ctn">
-            <div className="img-bg" />
-            {/* <img alt="doctor" className="doc-img" /> */}
+          <div className="doc-info-ctn">
+            {/* Image part */}
+            <div className="img-ctn">
+              {
+                docDetails.image === "" && <div className="img-bg" />
+              }
+              {
+                docDetails.image !== "" && <img className="doc-img" src={docDetails.image} alt="doctor"/>
+              }
+            </div>
+            <h5 className="doc-name">{docDetails.name}</h5>
+            <div className="doc-info-tbl">
+              <div className="doc-info-row">
+                <h5>Speciality </h5>
+                <h5>{docDetails.speciality}</h5>
+              </div>
+              <div className="doc-info-row white">
+                <h5>Cost </h5>
+                <h5>
+                  {docDetails.bill}
+                  {' '}
+                  $/h
+                </h5>
+              </div>
+              <div className="doc-info-row">
+                <h5>Location </h5>
+                <h5>{docDetails.location}</h5>
+              </div>
+              <div className="doc-info-row white">
+                <h5>Email </h5>
+                <h5>{docDetails.email}</h5>
+              </div>
+            </div>
+            <button className="reserve-btn" type="button" onClick={openReserve}>Reserve</button>
           </div>
-          <h5 className="doc-name">{docDetails.name}</h5>
-          <div className="doc-info-tbl">
-            <div className="doc-info-row">
-              <h5>Speciality </h5>
-              <h5>{docDetails.speciality}</h5>
-            </div>
-            <div className="doc-info-row white">
-              <h5>Cost </h5>
-              <h5>
-                {docDetails.bill}
-                {' '}
-                $/h
-              </h5>
-            </div>
-            <div className="doc-info-row">
-              <h5>Location </h5>
-              <h5>{docDetails.location}</h5>
-            </div>
-            <div className="doc-info-row white">
-              <h5>Email </h5>
-              <h5>{docDetails.email}</h5>
-            </div>
-          </div>
-          <button className="reserve-btn" type="button" onClick={openReserve}>Reserve</button>
-        </div>
         )
-        }
+      }
 
     </DoctorDetailsCtn>
   );
