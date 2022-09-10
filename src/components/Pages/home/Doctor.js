c7eb5ed32e562c6ac531d59085ac91a054502934
moise <moisemlg90@gmail.com>
2022-09-10 12:01:19 +0200

    move doctor styling

--- src/components/Pages/home/Doctor.js
+++ src/components/Pages/home/Doctor.js
@@ -1,70 +1,71 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import React from "react";
+import './doctor.css';
 
-const DoctorCtn = styled.div`
-  width: 350px;
-  height: 350px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  transition: all 100ms ease-in 0ms;
+// const DoctorCtn = styled.div`
+//   width: 350px;
+//   height: 350px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   transition: all 100ms ease-in 0ms;
 
-  .img-ctn {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 350px;
-    height: 350px;
-  }
+//   .img-ctn {
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     width: 350px;
+//     height: 350px;
+//   }
 
-  .doc-img {
-    width: 150px;
-    height: 150px;
-    margin: auto;
-    border-radius: 100%;
-  }
+//   .doc-img {
+//     width: 150px;
+//     height: 150px;
+//     margin: auto;
+//     border-radius: 100%;
+//   }
 
-  .img-bg {
-    width: 150px;
-    height: 150px;
-    margin: auto;
-    border-radius: 100%;
-    background: var(--img-background);
-  }
+//   .img-bg {
+//     width: 150px;
+//     height: 150px;
+//     margin: auto;
+//     border-radius: 100%;
+//     background: var(--img-background);
+//   }
 
-  .doc-name {
-    text-align: center;
-    font-family: "Ubuntu Light";
-    margin: 10px 0 0 0;
-    font-size: 1.2rem;
-  }
+//   .doc-name {
+//     text-align: center;
+//     font-family: "Ubuntu Light";
+//     margin: 10px 0 0 0;
+//     font-size: 1.2rem;
+//   }
 
-  .doc-speciality {
-    text-align: center;
-    font-family: "Ubuntu Thin";
-    margin: 20px 0 0 0;
-    color: var(--text-black);
-  }
+//   .doc-speciality {
+//     text-align: center;
+//     font-family: "Ubuntu Thin";
+//     margin: 20px 0 0 0;
+//     color: var(--text-black);
+//   }
 
-  .doc-cost {
-    text-align: center;
-    font-family: "Ubuntu Thin";
-    margin: 10px 0 0 0;
-    color: var(--text-black);
-  }
+//   .doc-cost {
+//     text-align: center;
+//     font-family: "Ubuntu Thin";
+//     margin: 10px 0 0 0;
+//     color: var(--text-black);
+//   }
 
-  :hover {
-    cursor: pointer;
-    background-color: rgba(227, 228, 227, 0.51);
-    border-radius: 10px;
-  }
-`;
+//   :hover {
+//     cursor: pointer;
+//     background-color: rgba(227, 228, 227, 0.51);
+//     border-radius: 10px;
+//   }
+// `;
 
 const Doctor = (props) => {
   const { name, speciality, cost, image } = props;
   return (
-    <DoctorCtn>
+    <div className="DoctorCtn">
       <div className="img-ctn">
         {
           image === "" && <div className="img-bg" />
@@ -81,7 +82,7 @@ const Doctor = (props) => {
         {' '}
         $/h
       </h5>
-    </DoctorCtn>
+    </div>
   );
 };
 
