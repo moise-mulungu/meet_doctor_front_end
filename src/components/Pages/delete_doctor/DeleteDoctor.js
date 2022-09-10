e7ca2a02ef48bd42bdcff42dd4a147bc3db09637
moise <moisemlg90@gmail.com>
2022-09-10 14:23:52 +0200

    Update styling to delete doctor

--- src/components/Pages/delete_doctor/DeleteDoctor.js
+++ src/components/Pages/delete_doctor/DeleteDoctor.js
@@ -2,137 +2,138 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { getUserDoctors, removeDoctor } from '../../../redux/doctors/doctorDelete';
+import './deleteDoctor.css';
 
-const DeleteDoctorCtn = styled.div`
+// const DeleteDoctorCtn = styled.div`
 
-  .doc-list-ctn {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    padding: 15px;
-    gap: 30px;
-  }
+//   .doc-list-ctn {
+//     display: flex;
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     padding: 15px;
+//     gap: 30px;
+//   }
 
-  .doc-ctn {
-    display: flex;
-    padding: 10px;
-    flex-direction: column;
-    justify-content: center;
-  }
+//   .doc-ctn {
+//     display: flex;
+//     padding: 10px;
+//     flex-direction: column;
+//     justify-content: center;
+//   }
 
-  .doc-name {
-    margin: 10px 0;
-    text-align: center;
-    font-family: "Ubuntu Light";
-    font-size: 1rem;
-  }
+//   .doc-name {
+//     margin: 10px 0;
+//     text-align: center;
+//     font-family: "Ubuntu Light";
+//     font-size: 1rem;
+//   }
 
-  .doc-speciality {
-    margin: 10px 0;
-    text-align: center;
-    font-family: "Ubuntu Thin";
-    font-size: 1rem;
-  }
+//   .doc-speciality {
+//     margin: 10px 0;
+//     text-align: center;
+//     font-family: "Ubuntu Thin";
+//     font-size: 1rem;
+//   }
 
-  .img-ctn {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 50px;
-    align-self: center;
-    width: 150px;
-    height: 150px;
-  }
+//   .img-ctn {
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     margin-top: 50px;
+//     align-self: center;
+//     width: 150px;
+//     height: 150px;
+//   }
 
-  .doc-img {
-    width: 150px;
-    height: 150px;
-    position: absolute;
-  }
+//   .doc-img {
+//     width: 150px;
+//     height: 150px;
+//     position: absolute;
+//   }
 
-  .img-bg {
-    width: 150px;
-    height: 150px;
-    margin: auto;
-    border-radius: 100%;
-    background: var(--img-background);
-  }
+//   .img-bg {
+//     width: 150px;
+//     height: 150px;
+//     margin: auto;
+//     border-radius: 100%;
+//     background: var(--img-background);
+//   }
   
-  .doc-img{
-    width: 150px;
-    height: 150px;
-    margin: auto;
-    border-radius: 100%;
-  }
+//   .doc-img{
+//     width: 150px;
+//     height: 150px;
+//     margin: auto;
+//     border-radius: 100%;
+//   }
 
-  .but-action-ctn {
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-  }
+//   .but-action-ctn {
+//     display: flex;
+//     justify-content: center;
+//     gap: 10px;
+//   }
 
-  .conf-ctn {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: 10px;
-  }
+//   .conf-ctn {
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: center;
+//     gap: 10px;
+//   }
 
-  .rem-ctn {
-    display: block;
-    width: 100px;
-    height: 100%;
-    color: white;
-    background: var(--red);
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    padding: 5px;
-    transition: all 300ms linear 0s;
-    :hover{
-      background: var(--red-darker);
-    }
-  }
+//   .rem-ctn {
+//     display: block;
+//     width: 100px;
+//     height: 100%;
+//     color: white;
+//     background: var(--red);
+//     border: none;
+//     border-radius: 4px;
+//     cursor: pointer;
+//     padding: 5px;
+//     transition: all 300ms linear 0s;
+//     :hover{
+//       background: var(--red-darker);
+//     }
+//   }
 
-  .cancel-but {
-    margin-right: 5px;
-    color: white;
-    background: var(--gray-simple);
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    padding: 5px;
-    transition: all 300ms linear 0s;
-    :hover{
-      background: var(--gray-darker);
-    }
-  }
+//   .cancel-but {
+//     margin-right: 5px;
+//     color: white;
+//     background: var(--gray-simple);
+//     border: none;
+//     border-radius: 4px;
+//     cursor: pointer;
+//     padding: 5px;
+//     transition: all 300ms linear 0s;
+//     :hover{
+//       background: var(--gray-darker);
+//     }
+//   }
 
-  .confirm-but {
-    margin-left: 5px;
-    color: white;
-    background: #f44d4d;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    padding: 5px;
-    transition: all 300ms linear 0s;
-    :hover{
-      background: var(--red-darker);
-    }
-  }
+//   .confirm-but {
+//     margin-left: 5px;
+//     color: white;
+//     background: #f44d4d;
+//     border: none;
+//     border-radius: 4px;
+//     cursor: pointer;
+//     padding: 5px;
+//     transition: all 300ms linear 0s;
+//     :hover{
+//       background: var(--red-darker);
+//     }
+//   }
 
-  .conf-ctn {
-    display: none;
-  }
-  .no-doc-ctn{
-    display: flex;
-    justify-content: center;
-  }
-  .no-doc{
-    align-content: center;
-  }
-`;
+//   .conf-ctn {
+//     display: none;
+//   }
+//   .no-doc-ctn{
+//     display: flex;
+//     justify-content: center;
+//   }
+//   .no-doc{
+//     align-content: center;
+//   }
+// `;
 
 function DeleteDoctor() {
   const doctors = useSelector((state) => state.deleteDoctor);
@@ -166,7 +167,7 @@ function DeleteDoctor() {
   }, []);
 
   return (
-    <DeleteDoctorCtn>
+    <div className="DeleteDoctorCtn">
       {
         doctors.length !== 0 ? (
         <div className="doc-list-ctn">
@@ -198,7 +199,7 @@ function DeleteDoctor() {
           <h5 className="no-doc">No doctor !</h5>
         </div>
       }
-    </DeleteDoctorCtn>
+    </div>
   );
 }
 
